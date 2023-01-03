1bcd340422ff3944a07c0124ffa87f5ef8eca084
amintf42 <mohammad.tasharofi0901@gmail.com>
2023-01-03 15:07:52 +0330

    second

--- 36/main.js
+++ 36/main.js
@@ -11,16 +11,33 @@ function addstudent() {
     let inputvalue = input.value;
     let li = document.createElement("li");
     let deletebtn = document.createElement("button");
+    deletebtn.addEventListener("click", removeStudent);
     deletebtn.textContent = "Delete";
     let text = document.createTextNode(inputvalue);
     li.appendChild(text);
     li.appendChild(deletebtn);
     ul.appendChild(li);
+    input.value = "";
+    input.focus();
     index++;
     span.textContent = index;
 }
 
+function showChar(e){
+    if(e.which === 13){
+        addstudent();
+    }
+}
+
+
+function removeStudent() {
+    let li = this.parentElement;
+    li.remove();
+    index--;
+    span.textContent = index;
+}
 
 
 // event
 btn.addEventListener("click", addstudent);
+input.addEventListener("keypress" , showChar)
